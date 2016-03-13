f8cd1f16f266f72725296708e93d345ba78ea263
Alexander Mann-Wahrenberg <p.a.mann@gmx.de>
2016-03-13 07:24:45 +0100

    Taking advantage of python's build-in variable __file__

--- PythonAndFTDI/SwitchDTR.py
+++ PythonAndFTDI/SwitchDTR.py
@@ -13,23 +13,23 @@ port = output.split()[3]
 
 # Error handling
 if port == '':
-    sys.stderr.write("%s:1:1: error: ECU is not connected via USB.\r\n" % (sys.argv[0]))
+    sys.stderr.write("%s:1:1: error: ECU is not connected via USB.\r\n" % __file__)
     exit(1)
 else:
-    sys.stderr.write("%s:1:1: info: ECU is connected via USB. Using virtual port %s\r\n" % (sys.argv[0], port))
+    sys.stderr.write("%s:1:1: info: ECU is connected via USB. Using virtual port %s\r\n" % (__file__, port))
     sys.stderr.flush()
 
 # Open connection
 ser = serial.Serial(port, 9600)
 if not ser.is_open:
-    sys.stderr.write("%s:1:2: error: Could not open virtual port %s\r\n" % (sys.argv[0], port))
+    sys.stderr.write("%s:1:2: error: Could not open virtual port %s\r\n" % (__file__, port))
     exit(1)
 else:
-    sys.stderr.write("%s:1:2: info: Successfully opened port %s\r\n" % (sys.argv[0], port))
+    sys.stderr.write("%s:1:2: info: Successfully opened port %s\r\n" % (__file__, port))
     sys.stderr.flush()
 
 # Hard reset MCU
-sys.stderr.write("%s:1:3: info: Starting test run by resetting ECU the hard way... " % (sys.argv[0]));
+sys.stderr.write("%s:1:3: info: Starting test run by resetting ECU the hard way... " % __file__);
 sys.stderr.flush()
 
 ser.setDTR(True)
@@ -40,7 +40,7 @@ sys.stderr.write("Started\r\n");
 sys.stderr.flush()
 
 # Redirect byte stream from serial to stdout
-sys.stderr.write("%s:1:4: info: Redirecting test-runner's output to stdout..." % (sys.argv[0]))
+sys.stderr.write("%s:1:4: info: Redirecting test-runner's output to stdout..." % __file__)
 sys.stderr.flush()
 # TODO: Implement this
 sys.stderr.write(" Finished\r\n")
